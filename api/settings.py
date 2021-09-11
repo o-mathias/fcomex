778a019f3db3ea63b095bd316a543f86265f1c93
o-mathias <mathiascbatista@gmail.com>
2021-09-11 13:23:30 -0300

    alteração do bd utilizado

--- api/settings.py
+++ api/settings.py
@@ -79,23 +79,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'oi',
-	'USER': 'root',
-	'PASSWORD': 'root',
-	'HOST': 'localhost',
-	'PORT': '3306',
     }
 }
 
